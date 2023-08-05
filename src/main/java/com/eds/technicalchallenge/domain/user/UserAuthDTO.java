abac4497e98330c58ee6c836fa7529fcda40c930
davidu2d <u2dtecnologia@gmail.com>
2023-08-05 19:11:44 -0300

    fix: add validations in dtos

--- src/main/java/com/eds/technicalchallenge/domain/user/UserAuthDTO.java
+++ src/main/java/com/eds/technicalchallenge/domain/user/UserAuthDTO.java
@@ -1,5 +1,6 @@
 package com.eds.technicalchallenge.domain.user;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
-public record UserAuthDTO(String username, String password){}
+public record UserAuthDTO(@NotBlank String username, @NotBlank String password){}
--- src/main/java/com/eds/technicalchallenge/domain/user/UserRegisterDTO.java
+++ src/main/java/com/eds/technicalchallenge/domain/user/UserRegisterDTO.java
@@ -1,8 +1,10 @@
 package com.eds.technicalchallenge.domain.user;
 
+import jakarta.validation.constraints.NotBlank;
+
 import java.time.LocalDateTime;
 
-public record UserRegisterDTO(String username, String password, UserRole role) {
+public record UserRegisterDTO(@NotBlank String username, @NotBlank String password, @NotBlank UserRole role) {
     public User toUser(){
         return new User(username, password, role, LocalDateTime.now());
     }
